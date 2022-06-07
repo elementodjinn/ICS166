13a504e91c1d4e1583b95b9e6107ceabb8f475c2
Rick <ruiheny1@uci.edu>
2022-06-07 05:12:41 -0700

    Highlight

--- ICS 166/Assets/Code/ItemHighlight.cs
+++ ICS 166/Assets/Code/ItemHighlight.cs
@@ -7,9 +7,6 @@ public class ItemHighlight : MonoBehaviour
     // Start is called before the first frame update
     private Color originalColor;
     public Vector2 item_pos, player_pos;
-    //private Vector2 item_pos = new Vector2(0f,0f);
-   // private Vector2 player_pos = new Vector2(0f,0f);
-    //public static float distance =  Vector2.Distance(item_pos, player_pos);
     private float range = 1f;
 
     void Start()
@@ -28,12 +25,10 @@ public class ItemHighlight : MonoBehaviour
         if (Vector2.Distance(item_pos, player_pos) <= range)
         { //Checking if player is near enough
             GetComponent<Renderer>().material.color = Color.blue;
-            Debug.Log(Vector2.Distance(item_pos, player_pos));
         }
         else
         {
             GetComponent<Renderer>().material.color = originalColor;
-            Debug.Log(Vector2.Distance(item_pos, player_pos));
         }
     }
 
---
+++ ICS 166/Assets/Code/MaskHighLight.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaskHighLight : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Color originalColor;
+    public Vector2 item_pos, player_pos;
+    private float range = 2.4f;
+
+    void Start()
+    {
+        originalColor = GetComponent<Renderer>().material.color;
+        GetComponent<Renderer>().material.color = originalColor;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        item_pos = transform.position;
+        player_pos = GameObject.Find("Body").transform.position;
+
+        if (Vector2.Distance(item_pos, player_pos) <= range)
+        { //Checking if player is near enough
+            GetComponent<Renderer>().material.color = new Color(1f, 1f, 1f, 4f);
+        }
+        else
+        {
+            GetComponent<Renderer>().material.color = originalColor;
+        }
+    }
+
+}
